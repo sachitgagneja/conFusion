3a6285795cca307639b002c323217e1c1c859284
Sachit Gagneja <skc7823@autuni.ac.nz>
2020-04-13 22:26:49 +1200

    SPA Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -7,7 +7,7 @@ import { MenuComponent } from '../menu/menu.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'contact', component: ContactComponent},
+    {path: 'contactus', component: ContactComponent},
     {path: 'about', component: AboutComponent},
     {path: 'menu', component: MenuComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
