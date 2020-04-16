c8c48d95db3f6acfefa3d416f793bbfbccc8be39
Sachit Gagneja <skc7823@autuni.ac.nz>
2020-04-16 13:09:48 +1200

    Dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { AppComponent } from './app.component';
+import { MatDialogModule } from "@angular/material";
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -20,6 +21,7 @@ import { LeaderService } from "./services/leader.service";
 import { DishService } from './services/dish.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -31,7 +33,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -42,12 +45,18 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule    
+    AppRoutingModule,
+    MatDialogModule 
   ],
   providers: [
     DishService,
     LeaderService
   ],
+
+  entryComponents: [
+    LoginComponent
+  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
