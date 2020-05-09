22cc3b17cce7979e5dab719076a6f2c2844e8a4b
Sachit Gagneja <skc7823@autuni.ac.nz>
2020-05-10 02:14:02 +1200

    Promise Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { DISHES } from '../shared/dishes';
 import { Dish } from '../shared/dish'
+import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,22 @@ import { Dish } from '../shared/dish'
 export class DishService {
 
   getDishes(): Promise<Dish[]>{
-    return Promise.resolve(DISHES);
+
+    return new Promise (resolve => {
+      setTimeout(() => resolve(DISHES), 2000);
+    });
   }
 
   getDish(id: String): Promise<Dish>{
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+    return new Promise (resolve => {
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedDish(): Promise<Dish>{
-    return Promise.resolve(DISHES.filter((dish) => (dish.featured))[0]);
+    return new Promise (resolve => {
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.featured))[0]), 2000);
+    })    
   }
-
   constructor() { }
 }
